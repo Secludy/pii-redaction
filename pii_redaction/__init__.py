77238ff592e643494baf56f5099017b620600c28
arcticfly <davidlcorbitt@gmail.com>
2025-03-22 12:59:49 -0700

    Reformat python code, update publish configuration

--- pii_redaction/__init__.py
+++ pii_redaction/__init__.py
@@ -1,4 +1,17 @@
-from .redactor import tag_pii_in_documents, clean_dataset, apply_tags, PIIHandlingMode, PIIType
+from .redactor import (
+    tag_pii_in_documents,
+    clean_dataset,
+    apply_tags,
+    PIIHandlingMode,
+    PIIType,
+)
 from .faker_utils import FakePIIGenerator
 
-__all__ = ['tag_pii_in_documents', 'clean_dataset', 'apply_tags', 'PIIHandlingMode', 'PIIType', 'FakePIIGenerator']
+__all__ = [
+    "tag_pii_in_documents",
+    "clean_dataset",
+    "apply_tags",
+    "PIIHandlingMode",
+    "PIIType",
+    "FakePIIGenerator",
+]
